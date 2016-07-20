48510ec069cc3a0e6dc22d82f0ad354b99e77e28
Henrique P Linhares <linhares.h@gmail.com>
2016-07-20 10:34:08 -0300

    adicionando a view v1

---
+++ Views/V1.sql
@@ -0,0 +1,16 @@
+/*
+View V1
+Para todas as aeronaves que possuem autorização para voar :
+Apresenta matricula da aeronave, data de fabricação da aeronave, modelo da aeronave, fabricante da aeronave e empresa proprietaria da aeronave.
+*/
+
+CREATE VIEW `Aeronaves_Em_Funcionamento` AS 
+SELECT Aeronave.matricula, Aeronave.datadefabricacao, 
+modelodeaeronave.nome as Nome_do_modelo, 
+fabricantedaaeronave.nome as Nome_do_fabricante, 
+empresadeaviacao.nome as Nome_da_empresa
+FROM Aeronave 
+JOIN autorizacaoparavoar ON Aeronave.idAeronave = autorizacaoparavoar.idAeronave
+JOIN modelodeaeronave ON Aeronave.idModeloDeAeronave = modelodeaeronave.idModeloDeAeronave
+JOIN fabricantedaaeronave ON fabricantedaaeronave.idFabricanteDaAeronave = modelodeaeronave.idFabricante
+JOIN empresadeaviacao ON Aeronave.idEmpresaDeAviacao = empresadeaviacao.idEmpresaDeAviacao;
