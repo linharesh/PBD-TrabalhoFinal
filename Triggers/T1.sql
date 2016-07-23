bb99708f1c80151a642709616341af56c3817b69
Henrique P Linhares <linhares.h@gmail.com>
2016-07-23 10:42:32 -0300

    versao entrega final

--- Triggers/T1.sql
+++ Triggers/T1.sql
@@ -1,6 +1,7 @@
 /*
 Trigger T1
-Quando houver uma atualização na tabela Voo, que marque o voo como concluído (idStatusDoVoo==7) 
+Quando houver uma atualização na tabela Voo, que marque o voo como em voo (idStatusDoVoo==6) 
+ou concluído (idStatusDoVoo==7) 
 então verificar se a aeronave utilizada no voo possui autorizacao para voar válida.
 Se não possuir, cancelar a atualizacao.
 Se possuir, chamar os procedures SP1, SP2 e SP3 e atualizar o registro
@@ -15,7 +16,7 @@ USE `trabalhofinal`$$
 CREATE DEFINER=`root`@`localhost` TRIGGER `trabalhofinal`.`voo_BEFORE_UPDATE` BEFORE UPDATE ON `voo` FOR EACH ROW
 BEGIN
 DECLARE num INT;
-if (new.idStatusDoVoo=7) then
+if (new.idStatusDoVoo=6 OR new.idStatusDoVoo = 7) then
 	
 	SELECT COUNT(*) from AutorizacaoParaVoar
 	where AutorizacaoParaVoar.idAeronave = old.idAeronave 
